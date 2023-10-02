d03ccd5893c9e459aadb68ead4e87b2501f5e96b
krosengr4 <rosenkev4@gmail.com>
2023-10-01 16:06:52 -1000

    Added inquire.prompt and put answers in a txt file.

--- Develope/index.js
+++ Develope/index.js
@@ -1,10 +1,34 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer'); 
+const fs = require('fs'); 
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = [];
 
-];
-
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
+  
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
@@ -13,3 +37,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+
